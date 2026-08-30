0db80958f2eb5e92c9ebbd0fed60f89f458669c9
Mohd Zaid <hlozaid18@gmail.com>
2026-08-31 01:05:09 +0530

    upedate

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 
+const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 
 main()
   .then(() => {
@@ -17,6 +17,22 @@ main()
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
 }
+
+let chat1 = new Chat({
+  from: "Zaid",
+  to: "Javed",
+  msg: "hello",
+  created_at: new Date(),
+});
+
+chat1
+  .save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Working");
